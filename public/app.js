b7d06f5c347785110770f0a4f219c88ad1c55de8
Sarah Murdock <sg.mdoc7@gmail.com>
2023-12-23 00:44:00 -0500

    UI

--- public/app.js
+++ public/app.js
@@ -1,2 +1,61 @@
-// app.js
-// Here you can write JavaScript to interact with your UI and send/receive data from the server
+// This line attaches an event listener to the form with the ID 'addItemForm'.
+// When the form is submitted, the specified function is called.
+document.getElementById('addItemForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevents the default form submission action.
+
+    // Retrieves the values entered into the input fields with IDs 'itemName' and 'itemQuantity'.
+    const itemName = document.getElementById('itemName').value;
+    const itemQuantity = document.getElementById('itemQuantity').value;
+
+    // Makes an HTTP POST request to the '/addItem' route on the server.
+    fetch('/addItem', {
+        method: 'POST', // Specifies the method as POST.
+        headers: {
+            'Content-Type': 'application/json', // Sets the content type of the request to JSON.
+        },
+        body: JSON.stringify({ name: itemName, quantity: itemQuantity }), // Sends the item name and quantity as JSON in the request body.
+    })
+    .then(response => response.json()) // Parses the JSON response from the server.
+    .then(data => {
+        loadInventory(); // Calls the function to reload and display the updated inventory.
+    });
+});
+
+// Similar to above, this attaches an event listener to the form for removing items.
+document.getElementById('removeItemForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevents the default form submission action.
+    const itemName = document.getElementById('removeItemName').value; // Retrieves the value of the item name to be removed.
+
+    // Makes an HTTP POST request to the '/removeItem' route on the server.
+    fetch('/removeItem', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: itemName }), // Sends the name of the item to be removed in the request body.
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('Item removed!'); // Alerts the user that the item was removed.
+        loadInventory(); // Reloads and displays the updated inventory.
+    });
+});
+
+// Function to load and display the current inventory.
+function loadInventory() {
+    fetch('/getInventory') // Makes a GET request to the '/getInventory' route on the server.
+    .then(response => response.json()) // Parses the JSON response.
+    .then(data => {
+        const itemList = document.getElementById('itemList'); // Gets the element where items will be displayed.
+        itemList.innerHTML = ''; // Clears the current list.
+
+        // Loops through each item in the data and creates a list item for each one.
+        data.forEach(item => {
+            let li = document.createElement('li'); // Creates a new list item element.
+            li.textContent = `${item.name}: ${item.quantity}`; // Sets the text content of the list item.
+            itemList.appendChild(li); // Adds the list item to the itemList element.
+        });
+    });
+}
+
+loadInventory(); // Calls the loadInventory function when the script is first executed.
--- server.js
+++ server.js
@@ -1,20 +1,130 @@
 // server.js
-const express = require('express');
-const XLSX = require('xlsx');
-const bodyParser = require('body-parser');
 
-const app = express();
-const port = 3000;
+// Importing necessary modules
+const express = require('express'); // Express framework for creating the server
+const XLSX = require('xlsx'); // Library for working with Excel files
+const bodyParser = require('body-parser'); // Middleware for parsing incoming request bodies
+const session = require('express-session'); // Import express-session
 
-// Middleware for parsing JSON and urlencoded data
-app.use(bodyParser.json());
+const app = express(); // Creating an instance of Express
+const port = 3000; // Setting the port number for the server
+
+// Set up session middleware
+app.use(session({
+    secret: 'your_secret_key', // Replace with a real secret in production
+    resave: false,
+    saveUninitialized: true,
+}));
+
+// Middleware setup for parsing request bodies
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from 'public' directory
-app.use(express.static('public'));
+// Helper function to read and write Excel files
+function readWriteExcel(action) {
+    const workbook = XLSX.readFile('inventory.xlsx'); // Reads the Excel file
+    const sheetName = workbook.SheetNames[0]; // Gets the first sheet name
+    const worksheet = workbook.Sheets[sheetName]; // Gets the worksheet
+    let data = XLSX.utils.sheet_to_json(worksheet); // Converts the worksheet data to JSON
 
-// Add more routes here as needed
+    action(data); // Executes the action function passed as an argument on the data
 
+    // Writing back to Excel
+    const newWorksheet = XLSX.utils.json_to_sheet(data); // Converts JSON data back to worksheet format
+    workbook.Sheets[sheetName] = newWorksheet; // Updates the workbook with the new worksheet
+    XLSX.writeFile(workbook, 'inventory.xlsx'); // Writes the updated workbook back to the Excel file
+}
+
+// Helper function to read users from Excel
+function getUsers() {
+    const workbook = XLSX.readFile('inventory.xlsx');
+    const sheetName = workbook.SheetNames.find(name => name === "Users"); // Find the 'Users' sheet
+    if (!sheetName) return []; // If 'users' sheet doesn't exist, return empty array
+    const worksheet = workbook.Sheets[sheetName];
+    return XLSX.utils.sheet_to_json(worksheet); // Convert sheet to JSON and return
+}
+
+// Route for handling login
+app.post('/login', (req, res) => {
+    const { name, pin } = req.body; // Extract name and pin from request body
+    const users = getUsers(); // Get the list of users
+
+    // Find the user with matching name and pin
+    // Convert both values to strings and trim them before comparison
+    // Also, use a case-insensitive comparison for the name
+    const user = users.find(u => 
+        u.name.toLowerCase().trim() === name.toLowerCase().trim() && 
+        u.pin.toString().trim() === pin.trim()
+    );
+
+    if (user) {
+        req.session.loggedIn = true; // Set a flag in the session
+        res.json({ success: true, message: 'Login successful' });
+    } else {
+        res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+});
+
+
+// Protect the main route
+app.get('/', (req, res) => {
+    if (req.session.loggedIn) {
+        res.sendFile(__dirname + '/public/index.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Serve the login page
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+// Route to get inventory items
+app.get('/getInventory', (req, res) => {
+    const workbook = XLSX.readFile('inventory.xlsx'); // Reads the Excel file
+    const sheetName = workbook.SheetNames[0]; // Gets the first sheet name
+    const worksheet = workbook.Sheets[sheetName]; // Gets the worksheet
+    const data = XLSX.utils.sheet_to_json(worksheet); // Converts the worksheet data to JSON
+    res.json(data); // Sends the JSON data as a response
+});
+
+// Route to add an item
+app.post('/addItem', (req, res) => {
+    const newItem = req.body; // Gets the new item data from the request body
+    readWriteExcel(data => {
+        const existingItem = data.find(item => item.name === newItem.name); // Looks for an existing item with the same name
+        if (existingItem) {
+            existingItem.quantity += parseInt(newItem.quantity); // If found, increases its quantity
+        } else {
+            data.push({ name: newItem.name, quantity: parseInt(newItem.quantity) }); // If not found, adds the new item
+        }
+    });
+    res.json({ message: 'Item added' }); // Sends a response indicating the item was added
+});
+
+// Route to remove an item
+app.post('/removeItem', (req, res) => {
+    const removeItem = req.body; // Gets the item to be removed from the request body
+    readWriteExcel(data => {
+        const index = data.findIndex(item => item.name === removeItem.name); // Finds the index of the item to be removed
+        if (index !== -1) {
+            data.splice(index, 1); // If the item exists, removes it from the data
+        }
+    });
+    res.json({ message: 'Item removed' }); // Sends a response indicating the item was removed
+});
+
+// Logout route
+app.get('/logout', (req, res) => {
+    req.session.destroy(); // Destroys session
+    res.redirect('/login'); // Redirects to the login page
+});
+
+// Static file serving, placed after session-based routes
+app.use(express.static('public')); 
+
+// Starting the server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
